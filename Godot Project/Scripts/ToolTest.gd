5f867edd84cce468e42448127691bd412fb3009b
Punguinz <78323799+Punguin1@users.noreply.github.com>
2021-08-12 19:45:18 -0400

    Add files via upload

---
+++ Godot Project/Scripts/ToolTest.gd
@@ -0,0 +1,13 @@
+tool
+extends Sprite
+
+
+
+
+func _process(delta):
+	rotation_degrees += 180 * delta
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
